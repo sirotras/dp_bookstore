fbc2feed80acbd3fc72b89fc7dd323233236ea80
Kyle S <kmsartoris@gmail.com>
2024-05-03 19:39:10 -0600

    bookstore start

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+# accounts/forms.py
+#imported CustomUser model via get_user_model which looks to our AUTH_USER_MODEL config in settings.py.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'email',
+            'username',
+        )
+        
+        
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'email',
+            'username',
+        )
